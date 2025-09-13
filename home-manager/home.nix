5c0e263af0875fb5c5d53f2ebbfca66d815b0ef6
kumar vaibhav <kv26102009@gmail.com>
2025-09-13 20:08:39 +0530

    starship.toml saved in st.toml

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,7 +5,42 @@
   home.username = "ijadux2";
   home.homeDirectory = "/home/ijadux2";
   home.enableNixpkgsReleaseCheck = false;
-  home.stateVersion = "25.05"; 
+  home.stateVersion = "25.05";
+  programs.zsh.enable = true;
+  programs.zsh.ohMyZsh.enable = true;
+  programs.zsh.ohMyZsh.theme = "agnoster";
+  programs.zsh.ohMyZsh.plugins = [ "git" "z" "sudo" "extract" ];
+  programs.git.enable = true;
+  programs.git.userName = "null";
+  programs.git.userEmail = "null";
+  programs.git.extraConfig = {
+    "pull.rebase" = "true";
+  };
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    package = pkgs.neovim.override {
+      withNodeJs = true;
+      withPython = true;
+      withRuby = false;
+      withPerl = false;
+      withLua = true;
+      withNix = true;
+    };
+    extraConfig = ''
+      set number
+      set relativenumber
+      set tabstop=4 
+      set shiftwidth=4  
+      set expandtab
+      set smartindent
+      set clipboard=unnamedplus
+      syntax on
+      filetype plugin indent on
+    '';
+  };
+   
   nixpkgs.config = {
   allowUnfree = true;
 };
